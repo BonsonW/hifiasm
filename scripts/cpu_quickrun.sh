e59808109a046038f10bb9ec4f5140d1e9bba20b
bonwon <bonson.ym@gmail.com>
2026-07-29 14:56:53 +1000

    add cpu quickrun script

--- scripts/cpu_quickrun.sh
+++ scripts/cpu_quickrun.sh
@@ -2,4 +2,4 @@
 
 test -d tmp && rm -r tmp
 mkdir tmp
-/usr/bin/time --verbose ./hifiasm -t1 -o tmp/temprun.asm ../data/chr22_20k.fastq 2> tmp/log
+/usr/bin/time --verbose ./hifiasm -t1 -o tmp/temprun.asm ../data/chr22_40k.fastq 2> tmp/log
